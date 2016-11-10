3f708af61028308103db4a30c50992c16381af99
webcrawlr <raymondwmuller2@gmail.com>
2016-11-09 16:05:32 -0800

    Added profile.h.
    This will generate the information not contained in the appearance: name, age, occupation, etc.  Currently adding the names.
    
    Currently unfinished.  Adding to the list would be greatly appreciated.

---
+++ profile.h
@@ -0,0 +1,9 @@
+#include <cstdlib>
+#include <string>
+#include <time.h>
+
+//256 most popular baby boy's names for 2016.
+//Source: http://www.babycenter.com/babyNamerSearch.htm?gender=MALE&includeExclude=INCLUDE&includeLimit=200&excludeLimit=100&meaning=&origin=&theme=&startsWith=&endsWith=&containing=&numberOfSyllables=&sort=
+
+
+string boyFirstNames[200] = {"Liam", "Noah", "Ethan", "Mason", "Lucas", "Logan", "Oliver", "Jackson", "Aiden", "Jacob", "Elijah", "James", "Benjamin", "Alexander", "Jack", "Luke", "William", "Carter", "Michael", "Daniel", "Owen", "Gabriel", "Henry", "Matthew", "Wyatt", "Jayden", "Nathan", "Isaac", "Ryan", "Caleb", "Sebastian", "David", "Andrew", "Dylan", "Samuel", "Connor", "Joshua", "Eli", "Hunter", "Jaxon", 
