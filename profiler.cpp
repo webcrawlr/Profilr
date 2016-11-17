11b56728508769e11506acca9dc7dabfd0a941b0
webcrawlr <raymondwmuller2@gmail.com>
2016-11-16 20:15:09 -0800

    Added a test run on the database.
    Needs debugging.

--- profiler.cpp
+++ profiler.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 #include <string>
+#include "database.h"
 
 using namespace std;
 
+int main(){
+    //For starters, let's just create the database.
+    int dbSize = 318000000; //Approximate size of the United States;
+    generateNewDatabase(dbSize);
+    addCreator();
+}
 
